89702511c14af10d3a1b28f309de0ae72473e7eb
Gilles <gilchen96@gmail.com>
2021-03-05 11:18:29 +0100

    Test for Five Different Books Reduction. Add setup and teardown before all and after all

--- src/test/java/books/BookShopTest.java
+++ src/test/java/books/BookShopTest.java
@@ -1,5 +1,7 @@
 package books;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,10 +15,16 @@ public class BookShopTest {
      * TODO : Put your test here
      */
 
+    BookShop shop;
+
+    @BeforeAll
+    public void setUp(){
+        BookShop shop = new BookShop("Harry Potter Shop");
+    }
+
 
     @Test
     public void myFirstTest(){
-        BookShop shop = new BookShop("Harry Potter Shop");
         int[] books = {1,2,4,0,3};
         // 4 Different Books -> Discount of 28%
         // 4*8 * 0,72 = 23,04 ----- Overflowing books = (0 + 1 + 3 + 0 + 2) * 8 = 48
@@ -27,7 +35,6 @@ public class BookShopTest {
 
     @Test
     public void Three_Reduction(){
-        BookShop shop = new BookShop("Harry Potter Shop");
         int[] books = {7,4,0,5,0};
         // 3 Different Books -> Discount of 14%
         // 3*8 * 0,86 = 20,64 ----- Overflowing books = (6 + 3 + 0 + 4 + 0) * 8 = 104
@@ -35,4 +42,21 @@ public class BookShopTest {
 
         assertTrue(shop.cost(books) == 124.64);
     }
+
+    @Test
+    public void Five_Reduction(){
+        int[] books = {7,4,3,5,4};
+        // 5 Different Books -> Discount of 35%
+        // 5*8 * 0,65 = 26 ----- Overflowing books = (6 + 3 + 2 + 4 + 3) * 8 = 144
+        // Test should result a total of 26 + 144 = 170
+
+        assertTrue(shop.cost(books) == 170);
+    }
+
+    @AfterAll
+    public void tearDown(){
+        shop = null;
+    }
+
+
 }
